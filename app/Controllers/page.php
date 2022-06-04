d59295d6efe58d99ef904d0a9896146c8986061d
taufiqalif <aliftaufiq4@gmail.com>
2022-06-04 19:48:40 +0700

    Praktikum 11: PHP Framework (Codeigniter)

---
+++ app/Controllers/page.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class page extends BaseController
+{
+  public function about()
+  {
+    echo "Ini halaman About";
+  }
+  public function contact()
+  {
+    echo "Ini halaman Contact";
+  }
+  public function faqs()
+  {
+    echo "Ini halaman FAQ";
+  }
+}
